0b23ba1c055f57735fbb90b6291878bdb2d2ccee
Saul Farber <saul@brainwire.net>
2017-08-10 02:17:53 -0400

    push_image scripts

---
+++ sample_app/web/push_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+REPO_NAME=sampleapp-web
+REPO_URL=015204734367.dkr.ecr.us-east-1.amazonaws.com
+cmd=$( aws ecr get-login --region us-east-1 )
+$cmd
+docker build -t $REPO_NAME:latest .
+docker tag $REPO_NAME:latest $REPO_URL/$REPO_NAME:latest
+docker push $REPO_URL/$REPO_NAME:latest
