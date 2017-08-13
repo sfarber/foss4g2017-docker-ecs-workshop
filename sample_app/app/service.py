f1f6c605015b270357d919f60030ee2c22f8b73b
Saul Farber <saul@brainwire.net>
2017-08-12 23:04:17 -0400

    pre-flight CORS headers

--- sample_app/app/service.py
+++ sample_app/app/service.py
@@ -5,11 +5,14 @@ import psycopg2
 import falcon
 
 class Handler(object):
+	# def on_options(self, req, resp):
+	# 	resp.append_header('Access-Control-Allow-Origin','*')
 	def on_post(self, req, resp):
 		self.do(req, resp)
 	def on_get(self, req, resp):
 		self.do(req. resp)
 	def do(self, req, resp):
+		resp.append_header('Access-Control-Allow-Origin','*')
 		urlparams = falcon.util.uri.parse_query_string(req.query_string)
 		source = urlparams['source']
 		target = urlparams['target']
