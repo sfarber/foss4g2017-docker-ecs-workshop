e1290c0e160c586eb48254663fcb19f4902b0021
Saul Farber <saul@brainwire.net>
2017-08-15 10:44:34 -0400

    make sure --no-include-email is included

--- sample_app/db/push_image.sh
+++ sample_app/db/push_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO_NAME=sampleapp-db
 REPO_URL=$( aws ecr get-authorization-token  | grep Endpoint | cut -d'"' -f 4 | sed -e 's/https:\/\///' )
-cmd=$( aws ecr get-login --region us-east-1)
+cmd=$( aws ecr get-login --region us-east-1 --no-include-email)
 $cmd
 docker build -t $REPO_NAME:latest .
 docker tag $REPO_NAME:latest $REPO_URL/$REPO_NAME:latest
